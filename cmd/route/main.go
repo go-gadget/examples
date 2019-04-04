d2f89b5a38bbd1bb038d93789a527bc80cd8a48c
Ivo van der Wijk <github@in.m3r.nl>
2019-04-04 14:01:17 +0200

    update

--- cmd/route/main.go
+++ cmd/route/main.go
@@ -7,11 +7,31 @@ import (
 	"github.com/go-gadget/gadget/vtree"
 )
 
+type User struct {
+	Name    string
+	Posts   []string
+	Address string
+}
+
+/*
+ * Steps:
+ * - print :id
+ * - hardcoded list of users (ids)
+ * - <router-link component thingy?>
+ */
 func main() {
 	fmt.Println("Go Go Gadget!")
-	AppComponent := GenerateComponent("<div><h1>App</h1></div>", nil, nil)
+	AppComponent := GenerateComponent(`<div><h1>App</h1>
+    <ul>
+      <li> <router-link To="User" Id="123"> John Doe </router-link></li>
+      <li> <router-link To="UserProfile" Id="234"> Jane Doe Profile </router-link></li>
+      <li> <router-link To="UserPosts" Id="345"> Shane Doe Posts</router-link></li>
+    </ul>
+    <router-view></router-view></div>`, nil, nil)
 	HomeComponent := GenerateComponent("<div><h2>Home</h2></div>", nil, nil)
-	UserComponent := GenerateComponent("<div><h2>User</h2><router-view></router-view></div>", nil, nil)
+	// For now handle RouteParams as props. In a sense this is more consistent and allows you to call the component
+	// without a route
+	UserComponent := GenerateComponent(`<div><h2>User</h2>Id: <span g-value="id">?</span><router-view></router-view></div>`, nil, []string{"id"})
 	UserProfile := GenerateComponent("<div><h2>Profile</h2></div>", nil, nil)
 	UserPosts := GenerateComponent("<div><h2>Posts</h2></div>", nil, nil)
 
@@ -41,7 +61,7 @@ func main() {
 		},
 	})
 
+	g.Mount(g.NewComponent(AppComponent))
 	go g.MainLoop()
-	g.Mount(NewComponent(AppComponent))
 	select {}
 }
